1be8bd70170e909d1ef2bbb70c33969a88ce7c72
Indervir Singh <iindervirsingh@gmail.com>
2021-01-20 21:30:02 -0800

    the page now loads everything, but no real user functionality yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,19 @@ $(document).ready(function () {
 
     // These are the questions that will be asked
     const quizQuestions = [ 
+
         "What is JavaScript?",
         "Which one of these is NOT a data type in Javascript?",
         "What grade should Indervir get on this quiz?"
+    ];
+
+    // Each index has 3 choices, which should correlate to the index of quizQuestions
+    const choices = [
+
+        ["Modern Programming Language", "Java Knock Off", "Ice Cream"],
+        ["Boolean", "String", "Function"],
+        ["F", "A", "D"]
+
     ];
 
 
@@ -23,16 +33,46 @@ $(document).ready(function () {
 
         // Create a new div which displays the current question
         var questionDiv = $("<div>");
-        questionDiv.addClass("question");
+
+        // Add a class to it, for later styling use
+        questionDiv.addClass("questions");
+
+        // Add the question text
         questionDiv.text(question);
 
 
         // Now add this div to the actual HTML page
         displayDiv.append(questionDiv);
-    }
+
+    };
+
+    var loadChoices = function (questionNumber) {
+
+        // Create some choices using buttons
+        // This loop will iterate 3 times, because only 3 choices
+        // It will only iterate on the subarray of the correlating question/choice
+        // EX: if questionNumber 2, loop through the 3 choices on choices[2]
+        for (let i = 0; i < 3; i++) {
+
+            // This is the actual choice text
+            var currentChoice = choices[questionNumber][i];
+
+            // Create a button which represents each choice
+            var choice = $("<button>");
+
+            // Add a class to it, for later styling use
+            choice.addClass("choices");
+
+            // Add the text to the button
+            choice.text(currentChoice);
+
+            // Add the button to the display-div so it shows on screen
+            displayDiv.append(choice);
+
+        }
 
 
-
+    };
 
 
 
@@ -46,14 +86,18 @@ $(document).ready(function () {
 
 
     // Loop through each question and display it 
-    for (let i = 0; i < quizQuestions.length; i++) {
+    for (let questionNumber = 0; questionNumber < quizQuestions.length; questionNumber++) {
 
         // Keep the current question here for easy readability
-        const currentQuestion = quizQuestions[i];
+        const currentQuestion = quizQuestions[questionNumber];
 
-        // This function is where the questions will be loaded
-        loadQuestion(currentQuestion, i);
+        // This function will display the question on screen
+        loadQuestion(currentQuestion, questionNumber);
 
+        // This function will display the correlating choices on screen
+        loadChoices(questionNumber);
+
+        // Create conditions
 
 
 
